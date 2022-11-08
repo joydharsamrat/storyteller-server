9ad086df4d88491f057a414b34d32e43b72a2ab7
joydharsamrat <joydharsamrat@gmail.com>
2022-11-08 12:44:23 +0600

    all services api created

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ async function run() {
 
         app.get('/services', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const options = { sort: { price: 1 } }
+            const cursor = serviceCollection.find(query, options);
             const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
+
+        app.get('/allServices', async (req, res) => {
+            const query = {};
+            const options = { sort: { price: 1 } }
+            const cursor = serviceCollection.find(query, options);
+            const services = await cursor.toArray();
             res.send(services)
         })
     }
